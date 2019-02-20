b6418ce4c94991ce81dcb4b20ac0dd3f7cc4ac48
Charlotte Fisher <charlotte.h.fisher@gmail.com>
2019-02-20 19:27:27 +0000

    Add Hello world
    added my first repository comment

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+def hello_world():
+    print("Hello World!")
+
+hello_world()
+hello_world()
+hello_world()
+
+def add_two_number():
+    number1 = 1
+    number2 = 2
+    answer = number1 + number2
+    print("{} plus {} is {}".format(number1,number2,answer))
+
+add_two_number()
